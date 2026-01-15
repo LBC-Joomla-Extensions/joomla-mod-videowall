fca7a05d4dd614ed1e0c82110b73be5efc998c8e
luinux81 <luinux81@gmail.com>
2026-01-15 22:00:46 +0000

    Housekeeping

--- src/helper.php
+++ src/helper.php
@@ -1,28 +1,16 @@
-
-<?php 
+<?php
 /*Este archivo puede considerarse el modelo (MVC) del módulo*/
 
-defined("_JEXEC") or die; 
+defined("_JEXEC") or die;
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-use Dickinsonjl\Lorum\Lorum;
 
-class modVideoTitulo{
+class modVideoTitulo
+{
 
-    public static function getParams(&$params){
+    public static function getParams(&$params)
+    {
         return $params;
     }
-    
 }
-
-class modBase{
-
-    public static function lorem(){
-        $lorum = new Lorum();
-        $lorum->buildCache();
-
-        return $lorum->giveMeParagraph();
-    }
-}
-?>
